97b2f257fc6056cca3f8ebd490eff0f146460f23
Nicolas Sanchez <1193447674@u.icesi.edu.co>
2022-05-16 08:09:10 -0500

    GH Pages

--- scripts/login.js
+++ scripts/login.js
@@ -4,33 +4,50 @@ const db = firebase.firestore();
 
 const user = localStorage.getItem("user");
 
-document.querySelector(".input-login").addEventListener('click', function (event) {
-    if (validateUser(document.querySelector("#email").textContent)) {
-        event.preventDefault();
-        alert("Logueo exitoso");
-        window.location.href = "../index.html";
-    } else {
-        alert("Contrasena incorrecta");
-    }
-});
+const formLogin = document.querySelector(".formLogin");
 
-const validateUser = async (email, password) => {
-    let existe = false;
-    await db.collection("students").where('correo', '==', email).get().then(async (snapshot) => {
-        let st = snapshot.docs[0];
-        if (st !== undefined) {
-            localStorage.setItem("user", JSON.stringify(st));
-            existe = true
+if(!user){
+    formLogin.addEventListener('submit', async function (event) {
+        event.preventDefault();
+        if (await validateUser(document.querySelector("#email").value, document.querySelector("#password").value )) {
+            alert("Logueo exitoso");
+            window.location.href = "../index.html";
+        } else {
+            alert("Contrasena incorrecta");
         }
     });
-    return existe;
+    
+    const validateUser = async (email, password) => {
+
+        let existe = false;
+        await db.collection("users").get().then(async res => await res.forEach(doc => {
+            if(doc.data().correo == email){
+                if(doc.data().contrasena == password){  
+
+                    existe =true;
+
+                    let user = {
+                        correo: doc.data().correo
+                    }
+                    localStorage.setItem("user", JSON.stringify(user));
+                }
+                
+            }
+        }));
+
+        return existe;
+    }
+    
+    document.querySelector(".input-register").addEventListener('click',function(event){
+        event.preventDefault();
+        goRegister();
+    });
+    
+    function goRegister(){
+        window.location.href= "../paginas/register.html";
+    }    
+
+}else{
+    window.location.href = "../paginas/index.html";
 }
 
-document.querySelector(".input-register").addEventListener('click',function(event){
-    event.preventDefault();
-    goRegister();
-});
-
-function goRegister(){
-    window.location.href= "../paginas/register.html";
-}
