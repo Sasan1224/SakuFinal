4d0d92f3ac2e83ba14751e02d973fd3bdd1142ab
Nicolas Sanchez <1193447674@u.icesi.edu.co>
2022-05-16 07:02:00 +0100

    GH Pages

--- root/scripts/login.js
+++ root/scripts/login.js
@@ -4,28 +4,33 @@ const db = firebase.firestore();
 
 const user = localStorage.getItem("user");
 
-if(!user){
-    document.querySelector(".input-login").addEventListener('click',function(event){
-        if(validateUser(document.querySelector("#email").textContent)){
-            event.preventDefault();
-            alert("Logueo exitoso");
-            window.location.href = "../index.html";
-        }else{
-            alert("Contrasena incorrecta");
+document.querySelector(".input-login").addEventListener('click', function (event) {
+    if (validateUser(document.querySelector("#email").textContent)) {
+        event.preventDefault();
+        alert("Logueo exitoso");
+        window.location.href = "../index.html";
+    } else {
+        alert("Contrasena incorrecta");
+    }
+});
+
+const validateUser = async (email, password) => {
+    let existe = false;
+    await db.collection("students").where('correo', '==', email).get().then(async (snapshot) => {
+        let st = snapshot.docs[0];
+        if (st !== undefined) {
+            localStorage.setItem("user", JSON.stringify(st));
+            existe = true
         }
     });
-    
-    const validateUser = async (email,password) => {
-        let existe = false;
-        await db.collection("students").where('correo', '==', email).get().then(async (snapshot) => {
-          let st = snapshot.docs[0];
-          if(st!==undefined) {
-                  localStorage.setItem("user",JSON.stringify(st));
-                  existe = true
-          }
-        });
-        return existe;
-    }
-}else{
-    window.location.href = "../index.html";
+    return existe;
+}
+
+document.querySelector(".input-register").addEventListener('click',function(event){
+    event.preventDefault();
+    goRegister();
+});
+
+function goRegister(){
+    window.location.href= "../paginas/register.html";
 }
